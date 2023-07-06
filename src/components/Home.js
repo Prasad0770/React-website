e2ad01cbf32bc74109a2046b5e6082aae3b49ec5
Prasad <prasadb21extc@student.mes.ac.in>
2023-07-06 23:08:08 +0530

    button

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,18 +2,22 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div
-      name="home"
-      className="h-screen w-full "
-    >
+    <div name="home" className="h-screen w-full">
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full text-center">
           <p className="text-lg sm:text-2xl font-bold text-white">
             Hey, I am
           </p>
           <h2 className="text-6xl sm:text-8xl font-bold text-white">
-            Prasad Bhor 
+            Prasad Bhor
           </h2>
+          <a
+            href="/path/to/your/cv.pdf"
+            download="Prasad_Bhor_CV.pdf"
+            className="mt-4 bg-purple-700 hover:bg-white text-black hover:scale-110 duration-300 font-bold py-2 px-4"
+          >
+            Download CV
+          </a>
         </div>
       </div>
     </div>
@@ -21,3 +25,5 @@ const Home = () => {
 };
 
 export default Home;
+
+
